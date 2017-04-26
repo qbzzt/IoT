b717c2c42c18f5dae59a898f5e3ce4a86c96028e
Ori Pomerantz <qbzzt1@gmail.com>
2017-04-26 21:15:15 +0000

    the INPUT table of iptables gets the right commands

--- Securing_Raspberry_PI_in_Your_Device/firewall_rules.js
+++ Securing_Raspberry_PI_in_Your_Device/firewall_rules.js
@@ -9,6 +9,20 @@ var fs = require("fs");
 var behaviorPattern;
 
 
+// The Firewall rules, as a 
+var fwRules = 
+	"#! /bin/bash\n" + 
+	"#\n" + 
+	"iptables -F INPUT\n" + 
+	"iptables -F OUTPUT\n\n" + 
+	"iptables -P INPUT DROP\n" +
+	"iptables -P OUTPUT ACCEPT\n" + 
+	"iptables -A INPUT -i lo -j ACCEPT\n" +
+	"iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT\n"; 
+
+
+
+
 fs.readFile("behaviorPattern.json", function(err, data) {
 	behaviorPattern = JSON.parse(data);
 
@@ -35,5 +49,15 @@ var processListenPorts = function(listenPorts) {
 			portByProto[proto][portByProto[proto].length] = portNum;
 	}
 
-	console.log(portByProto);
+	// For now, only deal with TCP ports
+	var tcpPorts = portByProto.tcp;
+	for(var j=0; j<tcpPorts.length; j++)
+		fwRules += 
+			"iptables -A INPUT -p tcp -m state " + 
+				"--state NEW -m tcp --dport " +
+				tcpPorts[j]  +" -j ACCEPT\n"
+
+	fwRules += "iptables -A INPUT -j DROP\n\n";
+
+	console.log(fwRules);
 };
