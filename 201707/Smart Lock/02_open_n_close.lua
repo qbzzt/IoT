b1ceace3b45e29505a07c79c8ce82982a041c39b
qbzzt <qbzzt1@gmail.com>
2017-07-02 22:40:00 -0500

    Create 02_open_n_close.lua

---
+++ 201707/Smart Lock/02_open_n_close.lua
@@ -0,0 +1,18 @@
+pin = 2
+pinVal = 0
+gpio.mode(pin, gpio.OUTPUT)
+
+counter = 0
+
+timer = tmr.create()
+
+timer:register(1000, tmr.ALARM_AUTO, 
+    function (t) 
+        gpio.write(pin, pinVal)
+        print ("Wrote to D" .. pin .. " value " .. pinVal)
+        pinVal = 1-pinVal
+        counter = counter + 1
+        if counter == 10 then t:stop(); t:unregister() end
+    end)
+
+timer:start()
