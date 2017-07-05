1211439d1d64825b64c026a4d27b6e28d4e5fb8a
qbzzt <qbzzt1@gmail.com>
2017-07-05 18:23:26 -0500

    Update 05_keygen.lua

--- 201707/Smart Lock/05_keygen.lua
+++ 201707/Smart Lock/05_keygen.lua
@@ -1,12 +1,20 @@
 secret = "hello"
+keyNum = 5
+
 key = secret
 
-for i=0,5 do
+for i=1,5 do
    key = crypto.toBase64(crypto.hash("sha1", key))
    print ("Key #" .. i .. " is " .. key)
 end
 
 storeMe = crypto.toBase64(crypto.hash("sha1", key))
 
-print ("Store this value: " .. storeMe)
+fd = file.open("authkey", "w+")
+fd:write(storeMe)
+fd:close()
 
+fd = file.open("keynum", "w+")
+fd:write(keyNum)
+fd:close
+   
