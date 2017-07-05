856c8b5ad54ad9164e71a18b737f4ee72f6ab3ff
qbzzt <qbzzt1@gmail.com>
2017-07-05 18:51:59 -0500

    Create 06_auth.lua

---
+++ 201707/Smart Lock/06_auth.lua
@@ -0,0 +1,24 @@
+
+function auth(newKey) do 
+  fd = file.open("authkey", "r")
+  local oldKey = fd:read()
+  fd:close()
+
+  fd.open("keynum", "r")
+  local keyNum = fd:read()
+  fd:close()
+
+  if oldKey == crypto.toBase64(crypto.hash("sha1", newKey)) then
+    -- Authentication successful
+    fd = file.open("authkey", "w+")
+    fd:write(newKey)
+    fd:close()
+    
+    fd = file.open("keynum", "w+")
+    fd:write(keyNum-1)
+    fd:close()
+    
+    return true
+  else
+    return false
+end    
