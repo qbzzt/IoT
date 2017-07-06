8d19de77440a6c0e540fa1ba136ef06f40262d48
qbzzt <qbzzt1@gmail.com>
2017-07-05 19:49:13 -0500

    Update 06_auth.lua

--- 201707/Smart Lock/06_auth.lua
+++ 201707/Smart Lock/06_auth.lua
@@ -1,24 +1,15 @@
 
 function auth(newKey) do 
-  fd = file.open("authkey", "r")
-  local oldKey = fd:read()
-  fd:close()
+  fd = file.open("authkey", "r"); local oldKey = fd:read(); fd:close()
 
-  fd.open("keynum", "r")
-  local keyNum = fd:read()
-  fd:close()
+  fd.open("keynum", "r"); local keyNum = fd:read(); fd:close()
 
   if oldKey == crypto.toBase64(crypto.hash("sha1", newKey)) then
     -- Authentication successful
-    fd = file.open("authkey", "w+")
-    fd:write(newKey)
-    fd:close()
-    
-    fd = file.open("keynum", "w+")
-    fd:write(keyNum-1)
-    fd:close()
-    
+    fd = file.open("authkey", "w+"); fd:write(newKey); fd:close()    
+    fd = file.open("keynum", "w+"); fd:write(keyNum-1); fd:close()    
     return true
   else
     return false
+  end
 end    
