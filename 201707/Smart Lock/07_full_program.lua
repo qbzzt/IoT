315eeae75894be191d6ab53f9eeb20fb4f2bb788
qbzzt <qbzzt1@gmail.com>
2017-07-06 22:31:44 -0500

    Update 07_full_program.lua

--- 201707/Smart Lock/07_full_program.lua
+++ 201707/Smart Lock/07_full_program.lua
@@ -31,7 +31,7 @@ function auth(newKey)
   fd = file.open("authkey", "r"); oldKey = fd:read(); fd:close()
   fd = file.open("keynum", "r"); keyNum = fd:read(); fd:close()
 
-  if (oldKey == crypto.toBase64(crypto.hash("sha1", newKey))) then
+  if (oldKey == crypto.toHex(crypto.hash("sha1", newKey))) then
     -- Authentication successful
     fd = file.open("authkey", "w+"); fd:write(newKey); fd:close()    
     fd = file.open("keynum", "w+"); fd:write(keyNum-1); fd:close()    
