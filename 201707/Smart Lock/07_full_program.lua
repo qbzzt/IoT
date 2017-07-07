c4bf5f7789d1351b2eee907d3811aff820d2e8c0
qbzzt <qbzzt1@gmail.com>
2017-07-06 20:27:56 -0500

    Update 07_full_program.lua

--- 201707/Smart Lock/07_full_program.lua
+++ 201707/Smart Lock/07_full_program.lua
@@ -23,10 +23,35 @@ if result then
     if result == false then print("wifi.ap.dhcp.start failed") end
 end
 
-function httpResponse(path)
-  local timer
 
-  if path == "/on" then
+function auth(newKey) 
+  local oldKey
+  local keyNum
+  
+  fd = file.open("authkey", "r"); oldKey = fd:read(); fd:close()
+  fd = file.open("keynum", "r"); keyNum = fd:read(); fd:close()
+
+  if (oldKey == crypto.toBase64(crypto.hash("sha1", newKey))) then
+    -- Authentication successful
+    fd = file.open("authkey", "w+"); fd:write(newKey); fd:close()    
+    fd = file.open("keynum", "w+"); fd:write(keyNum-1); fd:close()    
+    return true
+  else
+    return false
+  end
+end    
+
+
+
+function httpResponse(path)
+  local authCode
+
+  authCode = string.match(path, "/on%?key=(%S+)")
+
+  print (authCode)
+  
+  if auth(authCode) then
+    local timer
     gpio.write(pin, 1)
     timer = tmr.create()
 
@@ -36,7 +61,15 @@ function httpResponse(path)
     return "<h1>Lock opened for ten seconds</h1>"
   end
 
-  return "<1>Confused<1>"
+  -- If we got here, it means that the authorization
+  -- key exists, but is incorrect
+  if authCode then
+    return "<h1>Bad authorization key</h1>"
+  end
+
+  -- If we got here, there is no authorization key.
+  -- return the form
+  return "<h1>Confused</h1>"
 end
 
 httpServer = net.createServer(net.TCP)
@@ -45,6 +78,7 @@ httpServer:listen(80, function(conn)
       print(payload)
       path = string.match(payload, "%s+(%S+)%s+")
       resp = httpResponse(path)
+      print(resp)
       conn:send(resp)
    end)  -- Of the conn:on function
 end)   -- of the httpServer:listen function
