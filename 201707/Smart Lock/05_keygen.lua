3dca3bc4e2a0b48faacad8b8926b8cf48ea75608
qbzzt <qbzzt1@gmail.com>
2017-07-06 21:18:34 -0500

    Update 05_keygen.lua

--- 201707/Smart Lock/05_keygen.lua
+++ 201707/Smart Lock/05_keygen.lua
@@ -16,5 +16,14 @@ fd:close()
 
 fd = file.open("keynum", "w+")
 fd:write(keyNum)
-fd:close
+fd:close()
+
+
+-- Expected output (when the secret is "hello")
+-- Key #1 is qvTGHdzF6KLavt4PO0gs2a6pQ00=
+-- Key #2 is IgecUlSsmESIph6OzuOQswRIH9o=
+-- Key #3 is xZLg+fGiXRYG/jFRJFCF1/5YehY=
+-- Key #4 is lgJa/7lWrpTi7Fbl+Lss1J8ZEck=
+-- Key #5 is I7hEnawpXe2uKKVUDjcGj8f8zPw=
+
    
