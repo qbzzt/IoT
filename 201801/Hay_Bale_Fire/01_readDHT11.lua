ea6458c2f4d04768b9d2db58ca241be441481e3b
qbzzt <qbzzt1@gmail.com>
2017-12-31 21:06:33 -0600

    Update 01_readDHT11.lua

--- 201801/Hay_Bale_Fire/01_readDHT11.lua
+++ 201801/Hay_Bale_Fire/01_readDHT11.lua
@@ -1,1 +1,13 @@
+status, temp, humi = dht.read11(1)
+
+if status == dht.ERROR_TIMEOUT then
+   print("DHT timed out, is it connected correctly?")
+else
+    if status == dht.ERROR_CHECKSUM then
+        print("Checksum error, results probably valid")
+   end
+   print(string.format("Relative humidify:%4.2f %%\r\n", humi));
+   print(string.format("Temp: %4.2fC = %4.2fF\r\n",
+    temp, temp*5/9+32))
+end
 
