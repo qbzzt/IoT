62449b23aa5ba998b51666cb55776e0cd8684246
qbzzt <qbzzt1@gmail.com>
2018-01-07 20:32:54 -0600

    Update 04_LoRa_Send.ino

--- 201801/Hay_Bale_Fire/04_LoRa_Send.ino
+++ 201801/Hay_Bale_Fire/04_LoRa_Send.ino
@@ -1,13 +1,10 @@
+
 // Based on the tutorial at https://robotzero.one/heltec-wifi-lora-32/
 
 #include <SPI.h>
 #include <LoRa.h>
 
-
-#define SS      18
-#define RST     14
-#define DI0     26
-
+const int builtInLED = 25;
 
 typedef struct spi_pins {
   int sck;
@@ -21,15 +18,18 @@ typedef struct spi_pins {
 
 const spi_pins LORA_SPI_PINS = {.sck=5, .miso=19, .mosi=27, .cs=18, .rst=14, .irq=26};
 
-const MHz = 1000*1000;
+const int MHz = 1000*1000;
 
 void setup() {
+
+  pinMode(builtInLED, OUTPUT);
   
   SPI.begin(LORA_SPI_PINS.sck, LORA_SPI_PINS.miso, LORA_SPI_PINS.mosi, LORA_SPI_PINS.cs);
-  LoRa.setPins(LORA_SPI_PINS.cs, LORA_SPI_PINS.14, LORA_SPI_PINS.26);
+  LoRa.setPins(LORA_SPI_PINS.cs, LORA_SPI_PINS.rst, LORA_SPI_PINS.irq);
 
   // Initialize the serial device, wait until it is available
   Serial.begin(115200);
+  
   while (!Serial)
     ;
 
@@ -44,25 +44,21 @@ void setup() {
 }
 
 void loop() {
+  int sendingRetVal;
   LoRa.beginPacket();
-  LoRa.write("Hello", 5);
-  Serial.println("Ret val: %d", LoRa.endPacket());
+  LoRa.printf("Hello");
+  
+  sendingRetVal = LoRa.endPacket();
+  
+  Serial.printf("Sending success: %d\n", sendingRetVal );
 
-/*
-// try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
 
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
+  if (sendingRetVal) {
+    // Short pulse to show the message was sent
+    digitalWrite(builtInLED, HIGH);  
+    delay(100); 
+    digitalWrite(builtInLED, LOW); 
   }
-*/  
+
+  delay(1000);
 }
