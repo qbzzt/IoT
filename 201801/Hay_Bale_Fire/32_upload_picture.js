8d60890d9fb20808585af16497f5541626bf0a05
qbzzt <qbzzt1@gmail.com>
2018-06-30 19:07:46 -0500

    Create 32_upload_picture.js

---
+++ 201801/Hay_Bale_Fire/32_upload_picture.js
@@ -0,0 +1,72 @@
+// Credential and configuration for the IBM Cloud Object Storage
+const serviceAcctCred = {
+  << redacted >>
+};
+
+// The endpoint for the US, yours might be different
+const storageEndpoint = "s3-api.us-geo.objectstorage.softlayer.net";
+
+
+const fname = "/tmp/test.png";
+const bucket = "images4detection";
+
+
+
+const PiCamera = require('pi-camera');
+
+const myCamera = new PiCamera({
+	mode: 'photo',
+	output: fname,
+	nopreview: true
+});
+
+
+
+
+
+const ObjectStorageLib = require("ibm-cos-sdk");
+const objectStorage = new ObjectStorageLib.S3({
+	endpoint: storageEndpoint,
+	apiKeyId: serviceAcctCred.apikey,
+	ibmAuthEndpoint: 'https://iam.ng.bluemix.net/oidc/token',
+    serviceInstanceId: serviceAcctCred.resource_instance_id
+});
+
+
+const fs = require("fs");
+
+
+// The image file size is less than 5 MB, so there's no need for a 
+// multi-part upload
+const uploadImage = (key, callback) => {
+
+    fs.readFile(fname, (err, data) => { 
+		if (err) {
+			console.log(`Error reading file ${fname}: ${err}`);
+			process.exit();
+		}
+		  	
+		objectStorage.putObject({
+			Bucket: bucket,
+			Key: key,
+			ACL: "public-read",
+			Body: data
+		}).promise()
+		.then(callback)
+		.catch(e => console.log("Image upload error: " + e))
+	});
+};
+
+
+
+
+
+myCamera.snap()
+	.then(uploadImage("picture.png", () => {
+		console.log("Done");
+		process.exit();
+	}))
+	.catch(err => console.log('myCamera.snap error ' + err));
+	
+	
+
