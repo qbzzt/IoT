7f7272655f323303d6561669c2f94f5d272025a9
qbzzt <qbzzt1@gmail.com>
2018-10-08 08:29:35 -0500

    Create 43_process_picture.js

---
+++ 201801/Hay_Bale_Fire/43_process_picture.js
@@ -0,0 +1,106 @@
+// Divide time into ten minute slices
+const timeDiff = 10*60*1000;
+
+// Threshold at which there's an alarm
+const threshold = 0.75;
+
+// Directory to store pictures
+const pictDir = "/tmp";
+
+const NodeWebcam = require( "node-webcam" );
+
+// Takes about 20 minutes to install
+const imgSSIM = require("img-ssim");
+
+// File manipulation
+const fs = require("fs");
+ 
+ 
+
+ 
+// Create webcam instance 
+const Webcam = NodeWebcam.create({
+	// The output can be png or jpeg
+    output: "png",
+ 
+	// Use the default device
+    device: false,
+ 
+	// The picture can be provided as a buffer or a
+	// base64 string - but in this case we want to
+	// store it in a file 
+    callbackReturn: "location"
+ 
+});  // NodeWebcam.create
+ 
+
+
+
+// Get a timestamp for the current slice of time
+const getTimeStamp = () => {
+	const now = Date.now();   // msec from start of epoch
+	const dateObj = new Date();
+	dateObj.setTime(Math.floor(now / timeDiff) * timeDiff);
+	
+	// A colon confuses the image comparison library
+	return `${dateObj.getHours()}_${dateObj.getMinutes()}`;
+};
+
+
+// Get the filename for a picture taken at this time
+const getFname = () => `${pictDir}/pict_${getTimeStamp()}.png`;
+
+
+// The filename of the new picture, the one in processing
+const newPictFname = `${pictDir}/new_pict.png`;
+
+
+
+// Processing: 
+// 1. Take a picture
+// 2. Check if there is already a picture for this time stamp. 
+// 3. If there is, compare the two pictures
+// 4. If different, raise the alarm and preserve the old picture.
+// 5. Put the new picture in the directory
+const processPicture = () => {
+	const currentPict = getFname();
+	
+	// Take new picture
+	Webcam.capture(newPictFname, (err, data) => {
+		if (err) {
+			console.log(`Webcam.capture error: ${err}`);
+			return ;  // Exit the function call
+		}
+		
+		if (fs.existsSync(currentPict)) {
+			imgSSIM(currentPict, newPictFname, {
+				enforceSameSize: false,
+				resize: true
+			}, (err, score) => {
+				if (err) {
+					console.log(`imgSSIM error: ${err}`);
+					return ;
+				};
+				
+				// If the score is too low, raise the alarm.
+				if (score < threshold)
+					console.log(`alert about ${currentPict} ${threshold}`);
+				
+				// Replace the picture with the new one.
+				fs.rename(newPictFname, currentPict, (err) => {
+					console.log(`Rename to ${currentPict}. Err: ${err}`);
+				});  // fs.rename
+			
+			});  // imgSSIM
+		} else {  // if (fs.existsSync())
+			fs.rename(newPictFname, currentPict, (err) => {
+				console.log(`NO ${currentPict} yet, rename. Err: ${err}`);
+			});  // fs.rename
+		} // if (fs.existsSync())
+		
+	});  // Webcam.capture
+};  // processPicture definition
+
+
+// Use crontab to run repeatedly
+processPicture();
