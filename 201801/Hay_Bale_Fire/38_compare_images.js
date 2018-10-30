bcf3060b02267f0aa09463c96c0c761b194d80d6
qbzzt <qbzzt1@gmail.com>
2018-10-29 19:20:32 -0500

    Update 38_compare_images.js

--- 201801/Hay_Bale_Fire/38_compare_images.js
+++ 201801/Hay_Bale_Fire/38_compare_images.js
@@ -1,38 +1,25 @@
-const NodeWebcam = require( "node-webcam" );
+const PiCamera = require('pi-camera');
+
+const myCamera = new PiCamera({
+	mode: 'photo',
+	output: '/tmp/test2.png',
+	nopreview: true
+});
 
 // Takes about 20 minutes to install
 const imgSSIM = require("img-ssim");
  
 
- 
-// Create webcam instance 
-const Webcam = NodeWebcam.create({
-	// The output can be png or jpeg
-    output: "png",
- 
-	// Use the default device
-    device: false,
- 
-	// The picture can be provided as a buffer or a
-	// base64 string - but in this case we want to
-	// store it in a file 
-    callbackReturn: "location"
- 
-});  // NodeWebcam.create
- 
- 
-// The type is appended automatically
-Webcam.capture("/tmp/pict2", (err, data) => {
-	if (err) {
-		console.log("Webcam.capture error " + err);
-		return;
-	}
-	
-	imgSSIM(
-		"/tmp/pict1.png",
-		"/tmp/pict2.png",
-    	(err, similarity) => {
+myCamera.snap()
+	.then(result => {
+		imgSSIM(
+		"/tmp/test.png",
+		"/tmp/test2.png",
+    		(err, similarity) => {
 			console.log(err || similarity);
-	});  // end of imgSSIM
-		
-}); // end of Webcam.capture
+		});  // end of imgSSIM			
+	})  // end of myCamera.snap()
+	.catch(err => console.log('Error ' + err));
+
+
+
